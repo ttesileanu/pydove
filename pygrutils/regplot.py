711dc9f11b14d5d484f5008979e98caca6b76c1a
Tiberiu Tesileanu <ttesileanu@flatironinstitute.org>
2021-01-12 16:34:45 -0500

    Reorganize code

--- pygrutils/regplot.py
+++ pygrutils/regplot.py
@@ -14,204 +14,6 @@ from matplotlib.collections import PathCollection
 from statsmodels.regression.linear_model import RegressionResults
 
 
-def scatter(
-    x: Union[None, str, pd.Series, Sequence] = None,
-    y: Union[None, str, pd.Series, Sequence] = None,
-    data: Optional[pd.DataFrame] = None,
-    dropna: bool = True,
-    x_estimator: Optional[Callable[[Sequence], float]] = None,
-    seed: Union[int, np.random.Generator, np.random.RandomState] = 0,
-    x_jitter: float = 0,
-    y_jitter: float = 0,
-    x_ci_kws: Optional[dict] = None,
-    ax: Optional[plt.Axes] = None,
-    **kwargs,
-) -> PathCollection:
-    """ Make a scatter plot with some Seaborn-like extensions.
-
-    This function basically performs the scatter-plot half of `sns.regplot`.
-
-    Parameters
-    ----------
-    x
-        Data for x-axis (independent variable). This can be the data itself, or a string
-        indicating a column in `data`.
-    y
-        Data for y-axis (dependent variable). This can be the data itself, or a string
-        indicating a column in `data`.
-    data
-        Data in Pandas format. `x` and `y` should be strings indicating which columns to
-        use for independent and dependent variable, respectively.
-    dropna
-        Drop any observations in which either `x` or `y` is not-a-number.
-    x_estimator
-        Group samples with the same value of `x` and apply an estimator to collapse all
-        of the corresponding `y`values to a single number. If `x_ci` is given, a
-        confidence interval for each estimate is also calculated and drawn.
-    seed
-        Seed or random number generator for jitter.
-    x_jitter
-        Amount of uniform random noise to add to the `x` variable. This is added only
-        for the scatter plot, not for calculating the fit line. It's most useful for
-        discrete data.
-    y_jitter
-        Amount of uniform random noise to add to the `y` variable. This is added only
-        for the scatter plot, not for calculating the fit line. It's most useful for
-        discrete data.
-    x_ci_kws 
-        Additional keyword arguments to pass to `plt.errorbar` for the error bars at
-        each unique value of `x` when `x_estimator` is used.
-    ax
-        Axes object to draw the plot onto, otherwise uses `plt.gca()`.
-    Any additional keyword arguments are passed to `plt.scatter`.
-
-    Returns the output from `plt.scatter`.
-    """
-    # handle some defaults
-    ax = plt.gca() if ax is None else ax
-
-    # figure out what color to use (unless we already know)
-    if "c" not in kwargs and "color" not in kwargs:
-        (h,) = ax.plot([], [])
-        kwargs["color"] = h.get_color()
-        h.remove()
-
-    # standardize data
-    x, y = _standardize_data(x, y, data, dropna=dropna)
-
-    # prepare data (jitter, grouping)
-    xs, ys, ys_err = _prepare_data(
-        x, y, seed=seed, x_jitter=x_jitter, y_jitter=y_jitter, x_estimator=x_estimator
-    )
-
-    # make the scatter plot
-    kwargs.setdefault("alpha", 0.8)
-    h = ax.scatter(xs, ys, **kwargs)
-
-    # draw the error bars, if they exist
-    if ys_err is not None:
-        x_ci_kws = {} if x_ci_kws is None else x_ci_kws
-        # XXX this won't work with vector color
-        if "c" in kwargs:
-            color = kwargs["c"]
-        else:
-            # if this wasn't given explicitly, it was set above
-            color = kwargs["color"]
-
-        x_ci_kws.setdefault("color", color)
-        x_ci_kws.setdefault("elinewidth", 2.0)
-
-        ax.errorbar(xs, ys, yerr=ys_err, ls="none", **x_ci_kws)
-
-    return h
-
-
-def fitplot(
-    fit_results: RegressionResults,
-    x_range: Optional[Tuple] = None,
-    x: Optional[Sequence] = None,
-    logx: bool = False,
-    ci: Optional[float] = 95,
-    n_points: int = 100,
-    ci_kws: Optional[dict] = None,
-    ax: Optional[plt.Axes] = None,
-    **kwargs,
-):
-    """ Plot a polynomial regression curve.
-
-    This uses the fitting results from `statsmodels`.
-
-    Parameters
-    ----------
-    fit_results
-        The results of a polynomial fit, in `statsmodels` format. That is, it is assumed
-        that the exogenous variables in the model are powers of either `x` or `log(x)`
-        (see the `logx` option below). Specifically:
-            exog[:, i] = (x or log(x)) ** (i + (1 - has_constant)) .
-        To infer the existence of a constant term, `fit_results.k_constant` is used. The
-        method `fit_results.get_prediction` is used to calculate the fit line and
-        confidence interval. If `x_range` and `x` are not provided, the range of
-        x-values for evaluating the fit is obtained from `fit_results.model.exog`. The
-        function uses `fit_results.params` to determine the order of the polynomial.
-    x_range
-        Tuple `(x_min, x_max)` indicating the range of x-values to use for the fit.
-    x
-        Sequence of values where to evaluate the fit line. When this is provided,
-        `x_range` and `n_points` are ignored.
-    logx
-        If true, assumes that the linear regression involves powers of `log(x)` instead
-        of `x`.
-    ci
-        Size of confidence interval to draw for the regression line (in percent). This
-        will be drawn using translucent bands around the regression line. Set to `None`
-        to avoid drawing the confidence interval.
-    n_points
-        Number of points to use for drawing the fit line and confidence interval.
-    ci_kws
-        Additional keyword arguments to pass to `plt.fillbetween` for the confidence
-        interval.
-    ax
-        Axes object to draw the plot onto, otherwise uses `plt.gca()`.
-    Additional keywords are passed to `plt.plot()`.
-    """
-    # figure out the order of the fit
-    has_constant = fit_results.k_constant
-    order = len(fit_results.params) - has_constant
-    
-    # figure out the x values to use
-    if x is None:
-        if x_range is None:
-            # find range from the model itself
-            exog = fit_results.model.exog
-            x_orig = exog[:, has_constant]
-            if not logx:
-                x_range = (np.min(x_orig), np.max(x_orig))
-            else:
-                x_range = (np.exp(np.min(x_orig)), np.exp(np.max(x_orig)))
-
-        x = np.linspace(x_range[0], x_range[1], n_points)
-
-    # build the matrix of predictor variables
-    exog_fit1 = x if not logx else np.log(x)
-    exog_fit = np.empty((len(x), order + has_constant))
-    for k in range(order + has_constant):
-        exog_fit[:, k] = exog_fit1 ** (k + (1 - has_constant))
-        
-    # calculate the predictions
-    pred = fit_results.get_prediction(exog_fit)
-    mu = pred.predicted_mean
-    if ci is not None:
-        std = np.sqrt(pred.var_pred_mean)
-        n_std = np.sqrt(2) * erfinv(ci / 100)
-        err = n_std * std
-    else:
-        err = None
-
-    # set up keywords for fit line
-    if "lw" not in kwargs and "linewidth" not in kwargs:
-        kwargs["lw"] = 2.0
-
-    # draw the fit line
-    (h,) = ax.plot(x, mu, **kwargs)
-    color = h.get_color()
-
-    # set up keywords for confidence interval
-    ci_kws = {} if ci_kws is None else ci_kws
-    ci_kws.setdefault("alpha", 0.15)
-    if "ec" not in ci_kws and "edgecolor" not in ci_kws:
-        ci_kws["ec"] = "none"
-
-    # use same color for confidence interval as for fit line (unless overridden)
-    if not any(
-        _ in ci_kws for _ in ["c", "color", "facecolor", "facecolors", "fc"]
-    ):
-        ci_kws["facecolor"] = color
-
-    # draw confidence interval, if available
-    if err is not None:
-        ax.fill_between(x, mu - err, mu + err, **ci_kws)
-
-
 def regplot(
     x: Union[None, str, pd.Series, Sequence] = None,
     y: Union[None, str, pd.Series, Sequence] = None,
@@ -433,6 +235,204 @@ def regplot(
         ax.plot(eval_x, pred.predicted_mean, **line_kws)
 
     return fit_results
+
+
+def scatter(
+    x: Union[None, str, pd.Series, Sequence] = None,
+    y: Union[None, str, pd.Series, Sequence] = None,
+    data: Optional[pd.DataFrame] = None,
+    dropna: bool = True,
+    x_estimator: Optional[Callable[[Sequence], float]] = None,
+    seed: Union[int, np.random.Generator, np.random.RandomState] = 0,
+    x_jitter: float = 0,
+    y_jitter: float = 0,
+    x_ci_kws: Optional[dict] = None,
+    ax: Optional[plt.Axes] = None,
+    **kwargs,
+) -> PathCollection:
+    """ Make a scatter plot with some Seaborn-like extensions.
+
+    This function basically performs the scatter-plot half of `sns.regplot`.
+
+    Parameters
+    ----------
+    x
+        Data for x-axis (independent variable). This can be the data itself, or a string
+        indicating a column in `data`.
+    y
+        Data for y-axis (dependent variable). This can be the data itself, or a string
+        indicating a column in `data`.
+    data
+        Data in Pandas format. `x` and `y` should be strings indicating which columns to
+        use for independent and dependent variable, respectively.
+    dropna
+        Drop any observations in which either `x` or `y` is not-a-number.
+    x_estimator
+        Group samples with the same value of `x` and apply an estimator to collapse all
+        of the corresponding `y`values to a single number. If `x_ci` is given, a
+        confidence interval for each estimate is also calculated and drawn.
+    seed
+        Seed or random number generator for jitter.
+    x_jitter
+        Amount of uniform random noise to add to the `x` variable. This is added only
+        for the scatter plot, not for calculating the fit line. It's most useful for
+        discrete data.
+    y_jitter
+        Amount of uniform random noise to add to the `y` variable. This is added only
+        for the scatter plot, not for calculating the fit line. It's most useful for
+        discrete data.
+    x_ci_kws 
+        Additional keyword arguments to pass to `plt.errorbar` for the error bars at
+        each unique value of `x` when `x_estimator` is used.
+    ax
+        Axes object to draw the plot onto, otherwise uses `plt.gca()`.
+    Any additional keyword arguments are passed to `plt.scatter`.
+
+    Returns the output from `plt.scatter`.
+    """
+    # handle some defaults
+    ax = plt.gca() if ax is None else ax
+
+    # figure out what color to use (unless we already know)
+    if "c" not in kwargs and "color" not in kwargs:
+        (h,) = ax.plot([], [])
+        kwargs["color"] = h.get_color()
+        h.remove()
+
+    # standardize data
+    x, y = _standardize_data(x, y, data, dropna=dropna)
+
+    # prepare data (jitter, grouping)
+    xs, ys, ys_err = _prepare_data(
+        x, y, seed=seed, x_jitter=x_jitter, y_jitter=y_jitter, x_estimator=x_estimator
+    )
+
+    # make the scatter plot
+    kwargs.setdefault("alpha", 0.8)
+    h = ax.scatter(xs, ys, **kwargs)
+
+    # draw the error bars, if they exist
+    if ys_err is not None:
+        x_ci_kws = {} if x_ci_kws is None else x_ci_kws
+        # XXX this won't work with vector color
+        if "c" in kwargs:
+            color = kwargs["c"]
+        else:
+            # if this wasn't given explicitly, it was set above
+            color = kwargs["color"]
+
+        x_ci_kws.setdefault("color", color)
+        x_ci_kws.setdefault("elinewidth", 2.0)
+
+        ax.errorbar(xs, ys, yerr=ys_err, ls="none", **x_ci_kws)
+
+    return h
+
+
+def fitplot(
+    fit_results: RegressionResults,
+    x_range: Optional[Tuple] = None,
+    x: Optional[Sequence] = None,
+    logx: bool = False,
+    ci: Optional[float] = 95,
+    n_points: int = 100,
+    ci_kws: Optional[dict] = None,
+    ax: Optional[plt.Axes] = None,
+    **kwargs,
+):
+    """ Plot a polynomial regression curve.
+
+    This uses the fitting results from `statsmodels`.
+
+    Parameters
+    ----------
+    fit_results
+        The results of a polynomial fit, in `statsmodels` format. That is, it is assumed
+        that the exogenous variables in the model are powers of either `x` or `log(x)`
+        (see the `logx` option below). Specifically:
+            exog[:, i] = (x or log(x)) ** (i + (1 - has_constant)) .
+        To infer the existence of a constant term, `fit_results.k_constant` is used. The
+        method `fit_results.get_prediction` is used to calculate the fit line and
+        confidence interval. If `x_range` and `x` are not provided, the range of
+        x-values for evaluating the fit is obtained from `fit_results.model.exog`. The
+        function uses `fit_results.params` to determine the order of the polynomial.
+    x_range
+        Tuple `(x_min, x_max)` indicating the range of x-values to use for the fit.
+    x
+        Sequence of values where to evaluate the fit line. When this is provided,
+        `x_range` and `n_points` are ignored.
+    logx
+        If true, assumes that the linear regression involves powers of `log(x)` instead
+        of `x`.
+    ci
+        Size of confidence interval to draw for the regression line (in percent). This
+        will be drawn using translucent bands around the regression line. Set to `None`
+        to avoid drawing the confidence interval.
+    n_points
+        Number of points to use for drawing the fit line and confidence interval.
+    ci_kws
+        Additional keyword arguments to pass to `plt.fillbetween` for the confidence
+        interval.
+    ax
+        Axes object to draw the plot onto, otherwise uses `plt.gca()`.
+    Additional keywords are passed to `plt.plot()`.
+    """
+    # figure out the order of the fit
+    has_constant = fit_results.k_constant
+    order = len(fit_results.params) - has_constant
+    
+    # figure out the x values to use
+    if x is None:
+        if x_range is None:
+            # find range from the model itself
+            exog = fit_results.model.exog
+            x_orig = exog[:, has_constant]
+            if not logx:
+                x_range = (np.min(x_orig), np.max(x_orig))
+            else:
+                x_range = (np.exp(np.min(x_orig)), np.exp(np.max(x_orig)))
+
+        x = np.linspace(x_range[0], x_range[1], n_points)
+
+    # build the matrix of predictor variables
+    exog_fit1 = x if not logx else np.log(x)
+    exog_fit = np.empty((len(x), order + has_constant))
+    for k in range(order + has_constant):
+        exog_fit[:, k] = exog_fit1 ** (k + (1 - has_constant))
+        
+    # calculate the predictions
+    pred = fit_results.get_prediction(exog_fit)
+    mu = pred.predicted_mean
+    if ci is not None:
+        std = np.sqrt(pred.var_pred_mean)
+        n_std = np.sqrt(2) * erfinv(ci / 100)
+        err = n_std * std
+    else:
+        err = None
+
+    # set up keywords for fit line
+    if "lw" not in kwargs and "linewidth" not in kwargs:
+        kwargs["lw"] = 2.0
+
+    # draw the fit line
+    (h,) = ax.plot(x, mu, **kwargs)
+    color = h.get_color()
+
+    # set up keywords for confidence interval
+    ci_kws = {} if ci_kws is None else ci_kws
+    ci_kws.setdefault("alpha", 0.15)
+    if "ec" not in ci_kws and "edgecolor" not in ci_kws:
+        ci_kws["ec"] = "none"
+
+    # use same color for confidence interval as for fit line (unless overridden)
+    if not any(
+        _ in ci_kws for _ in ["c", "color", "facecolor", "facecolors", "fc"]
+    ):
+        ci_kws["facecolor"] = color
+
+    # draw confidence interval, if available
+    if err is not None:
+        ax.fill_between(x, mu - err, mu + err, **ci_kws)
 
 
 def _standardize_data(
