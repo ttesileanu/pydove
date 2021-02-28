41cde877faa5ef4d4c7894e5ec29639973a99045
Tiberiu Tesileanu <ttesileanu@flatironinstitute.org>
2021-02-28 17:51:53 -0500

    Update test_color to use pydove name

--- test/test_color.py
+++ test/test_color.py
@@ -8,9 +8,9 @@
 #       format_version: '1.3'
 #       jupytext_version: 1.4.2
 #   kernelspec:
-#     display_name: pygrutils
+#     display_name: pydove
 #     language: python
-#     name: pygrutils
+#     name: pydove
 # ---
 
 # %% [markdown]
@@ -23,41 +23,41 @@
 import numpy as np
 import seaborn as sns
 
-import pygrutils as gr
+import pydove as dv
 
 # %% [markdown]
 # ## Basic use of `colorbar`
 
 # %%
 rng = np.random.default_rng(0)
-with gr.FigureManager() as (_, ax):
+with dv.FigureManager() as (_, ax):
     n = 500
     x = rng.uniform(size=n)
     y = rng.uniform(size=n)
     h = ax.scatter(x, y, c=y)
-    gr.colorbar(h)
+    dv.colorbar(h)
 
 # %% [markdown]
 # ## `colorbar` options
 
 # %%
 rng = np.random.default_rng(0)
-with gr.FigureManager() as (_, ax):
+with dv.FigureManager() as (_, ax):
     n = 500
     x = rng.uniform(size=n)
     y = rng.uniform(size=n)
     h = ax.scatter(x, y, c=x + y, cmap="Reds")
-    gr.colorbar(h, fraction=0.1, pad=1.0, location="left")
+    dv.colorbar(h, fraction=0.1, pad=1.0, location="left")
 
 # %% [markdown]
 # ## Custom gradient color map
 
 # %%
 rng = np.random.default_rng(0)
-with gr.FigureManager() as (_, ax):
+with dv.FigureManager() as (_, ax):
     h = ax.imshow(
-        rng.uniform(size=(20, 20)), cmap=gr.gradient_cmap("C0_to_C1", "C0", "C1")
+        rng.uniform(size=(20, 20)), cmap=dv.gradient_cmap("C0_to_C1", "C0", "C1")
     )
-    gr.colorbar(h)
+    dv.colorbar(h)
 
 # %%
