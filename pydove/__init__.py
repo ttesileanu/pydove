e3dbf9fc4bc4e6b62abae60deeec28c4a0644dcb
Tiberiu Tesileanu <ttesileanu@flatironinstitute.org>
2021-04-01 13:59:21 -0400

    Add package __version__

--- pydove/__init__.py
+++ pydove/__init__.py
@@ -2,3 +2,5 @@ from pydove.regplot import regplot, scatter, fitplot, polyfit
 from pydove.figure_manager import FigureManager
 from pydove.color import colorbar, gradient_cmap
 from pydove.plot import color_plot
+
+__version__ = "0.3.0"
