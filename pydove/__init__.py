ebf39db01ecb78f7bc8f174514aa0cb20fb9fcef
Tiberiu Tesileanu <ttesileanu@flatironinstitute.org>
2021-04-29 17:59:03 -0400

    Set InlineBackend bbox_inches to None by default

--- pydove/__init__.py
+++ pydove/__init__.py
@@ -1,3 +1,11 @@
+# if running in Jupyter with InlineBackend, ensure we don't crop or enlarge figures
+try:
+    from traitlets.config import get_config
+    c = get_config()
+    c.InlineBackend.print_figure_kwargs = {'bbox_inches': None}
+except:
+    pass
+
 from pydove.regplot import regplot, scatter, fitplot, polyfit
 from pydove.figure_manager import FigureManager
 from pydove.color import colorbar, gradient_cmap
